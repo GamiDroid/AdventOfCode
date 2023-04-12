6228db7f0897b74518eacb9296d8693ec7cab2bd
Kevin Damen <k.damen@bellt.nl>
2023-04-12 21:29:13 +0200

    Add a ChallengeHelper to get the resource file path

---
+++ AdventOfCode.ConsoleApp/ChallengeHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.ConsoleApp;
+internal class ChallengeHelper
+{
+    public static string GetResourceFilePath([CallerFilePath] string callerFilePath = "")
+    {
+        var challengeName = Path.GetFileNameWithoutExtension(callerFilePath)?.Split("_")[1];
+        var filePath = Path.Combine(callerFilePath, "..", "Resources", $"{challengeName}.txt");
+
+        if (!File.Exists(filePath))
+            File.Create(filePath);
+
+        return filePath;
+    }
+}
