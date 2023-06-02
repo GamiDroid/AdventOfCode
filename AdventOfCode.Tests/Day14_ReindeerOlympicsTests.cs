0531dbc9155aaa22ac3ddcc05f49e87441ac753a
Kevin Damen <k.damen@bellt.nl>
2023-06-02 17:28:40 +0200

    Implemented day 14 - Reindeer Olympics of year 2015

---
+++ AdventOfCode.Tests/Day14_ReindeerOlympicsTests.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode._2015;
+
+namespace AdventOfCode.Tests;
+public class Day14_ReindeerOlympicsTests
+{
+    [Fact]
+    public void Reindeer_Create_ShouldHaveNoDistanceTraveled()
+    {
+        // Arrange
+        var reindeer = new Day14_ReindeerOlympics.Reindeer("", 0, 0, 0);
+
+        // Act
+        var actual = reindeer.DistanceTraveled;
+
+        // Assert
+        Assert.Equal(0, actual);
+    }
+
+    [Fact]
+    public void Reindeer_ElapseTime_ShouldAddDistanceTraveled()
+    {
+        // Arrange
+        var expected = 5;
+        var reindeer = new Day14_ReindeerOlympics.Reindeer("", expected, 0, 0);
+
+        // Act
+        reindeer.ElapseTime();
+        var actual = reindeer.DistanceTraveled;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Reindeer_IsRunning_ShouldBeFalseWhenRunTimeIsUp()
+    {
+        // Arrange
+        var reindeer = new Day14_ReindeerOlympics.Reindeer("", 0, 1, 10);
+
+        // Act 
+        reindeer.ElapseTime(2);
+        var actual = reindeer.IsRunning;
+
+        // Assert 
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public void Reindeer_ElapseTime_ShouldNotIncrementDistanceTraveledWhenIsRunningIsFalse()
+    {
+        // Arrange
+        var reindeer = new Day14_ReindeerOlympics.Reindeer("", 5, 1, 10);
+
+        // Act 
+        reindeer.ElapseTime(3);
+
+        // Assert 
+        Assert.False(reindeer.IsRunning);
+        Assert.Equal(5, reindeer.DistanceTraveled);
+    }
+}
---
+++ AdventOfCode/2015/Day14_ReindeerOlympics.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015;
+
+[Challenge(2015, 14)]
+internal class Day14_ReindeerOlympics
+{
+    private string[] _lines = new string[0];
+
+    [Setup]
+    public void Setup()
+    {
+        var filePath = ChallengeHelper.GetResourceFilePath();
+        _lines = File.ReadAllLines(filePath);
+    }
+
+    private static ICollection<Reindeer> GetReindeers(string[] lines)
+    {
+        var reindeers = new List<Reindeer>();
+        foreach (var line in lines)
+        {
+            var match = Regex.Match(line, "(?<name>\\w+) can fly (?<speed>\\d+) km\\/s for (?<runTime>\\d+) seconds, but then must rest for (?<restTime>\\d+) seconds\\.");
+            if (match.Success)
+            {
+                var name = match.Groups["name"].Value;
+                var speed = int.Parse(match.Groups["speed"].Value);
+                var runTime = int.Parse(match.Groups["runTime"].Value);
+                var restTime = int.Parse(match.Groups["restTime"].Value);
+
+                var reindeer = new Reindeer(name, speed, runTime, restTime);
+                reindeers.Add(reindeer);
+            }
+        }
+
+        return reindeers;
+    }
+
+    [Part(1)]
+    public void ExecutePart01()
+    {
+        var reindeers = GetReindeers(_lines);
+        foreach (var reindeer in reindeers)
+        {
+            reindeer.ElapseTime(2503);
+            Console.WriteLine($"Reindeer {reindeer.Name} traveled {reindeer.DistanceTraveled} km.");
+        }
+
+        var winningDistance = reindeers.Max(r => r.DistanceTraveled);
+        Console.WriteLine($"The winning distance is {winningDistance} km.");
+    }
+
+    [Part(2)]
+    public void ExecutePart02()
+    {
+        var reindeers = GetReindeers(_lines);
+        for (int i = 0; i < 2503; i++)
+        {
+            foreach (var reindeer in reindeers)
+                reindeer.ElapseTime();
+            var leadDistance = reindeers.Max(r => r.DistanceTraveled);
+            var leadReindeers = reindeers.Where(r => r.DistanceTraveled == leadDistance).ToList();
+            foreach (var leadReindeer in leadReindeers)
+                leadReindeer.AddPoint();
+        }
+
+        foreach (var reindeer in reindeers)
+            Console.WriteLine($"Reindeer {reindeer.Name} has {reindeer.Points} points.");
+
+        var winningPoints = reindeers.Max(r => r.Points);
+        Console.WriteLine($"The winning points is {winningPoints} points.");
+    }
+
+    public record Reindeer(string Name, int Speed, int RunTime, int RestTime)
+    {
+        public int DistanceTraveled { get; private set; } = 0;
+        public bool IsRunning { get; private set; }
+        public int Points { get; private set; }
+
+        public void ElapseTime(int time)
+        {
+            for (int i = 0; i < time; i++)
+                ElapseTime();
+        }
+
+        public void ElapseTime()
+        {
+            if (_timer <= 0)
+            {
+                IsRunning = !IsRunning;
+                _timer = IsRunning ? RunTime : RestTime;
+            }
+
+            if (IsRunning)
+                DistanceTraveled += Speed;
+
+            _timer--;
+        }
+
+        public void AddPoint() => Points++;
+
+        private int _timer;
+    }
+}
