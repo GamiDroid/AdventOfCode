cf136340a89bd871d24c0bcfb3d3ef4167c67158
Kevin Damen <k.damen@bellt.nl>
2023-07-28 21:22:27 +0200

    Implemented part 2 of day 17 2015

--- AdventOfCode/2015/Day17_NoSuchThingAsTooMuch.cs
+++ AdventOfCode/2015/Day17_NoSuchThingAsTooMuch.cs
@@ -43,6 +43,16 @@ internal class Day17_NoSuchThingAsTooMuch
         Console.WriteLine("Amount of combinations: {0}", count);
     }
 
+    [Part(2)]
+    public void Part02()
+    {
+        Console.WriteLine($"There are {_containerSizes.Length} container sizes");
+
+        var count = CountMinCombinations(_containerSizes, 150);
+
+        Console.WriteLine("Amount of combinations: {0}", count);
+    }
+
     private static int CountCombinations(int[] containers, int neededSize)
     {
         var count = Permutations(containers, neededSize, 0, 0, 0);
@@ -69,5 +79,45 @@ internal class Day17_NoSuchThingAsTooMuch
         }
 
         return count;
+    }
+
+    private static int CountMinCombinations(int[] containers, int neededSize)
+    {
+        var containerCounts = MinPermutations(containers, neededSize, 0, 0, 0, new Dictionary<int, int>());
+
+        var minimumAmountOfContainers = containerCounts.Keys.Min();
+        var possibleCombinations = containerCounts[minimumAmountOfContainers];
+
+        return possibleCombinations;
+    }
+
+    private static IDictionary<int, int> MinPermutations(int[] containerSizes, int neededSize, int index, int size, int containerPicked, IDictionary<int, int> containerCounts)
+    {
+        if (index == containerSizes.Length || size >= neededSize)
+        {
+            if (size == neededSize)
+            {
+                if (!containerCounts.ContainsKey(containerPicked))
+                {
+                    containerCounts[containerPicked] = 1;
+                }
+                else
+                {
+                    containerCounts[containerPicked]++;
+                }
+
+                return containerCounts;
+            }
+        }
+        else
+        {
+            for (int i = index; i < containerSizes.Length; i++)
+            {
+                int newSize = size + containerSizes[i];
+                containerCounts = MinPermutations(containerSizes, neededSize, i + 1, newSize, containerPicked + 1, containerCounts);
+            }
+        }
+
+        return containerCounts;
     }
 }
