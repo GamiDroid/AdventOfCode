abb3fe4dfc5948830154f127310f0ccf696de787
Kevin Damen <k.damen@bellt.nl>
2023-08-16 18:00:08 +0200

    Solve day 20 part 1 of 2015

---
+++ AdventOfCode/2015/Day20_InfiniteElvesAndInfiniteHouses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2015;
+
+[Challenge(2015, 20)]
+internal class Day20_InfiniteElvesAndInfiniteHouses
+{
+    [Part(1)]
+    public void Part01()
+    {
+        var house = Solve();
+        Console.WriteLine($"House {house} is lowest number with gets presents");
+    }
+
+    private static int Solve()
+    {
+        var puzzleInput = 34000000;
+
+        // For part A we don't have to multiply, since we also divided the input by 10
+        var divide =  10;
+        var multiply = 1;
+
+        var input = puzzleInput / divide;
+        var houses = new int[input];
+
+        var results = new List<int>();
+        for (var elf = 1; elf < input; elf++)
+        {
+            var visits = 0;
+
+            for (var house = elf; house < input; house += elf)
+            {
+                houses[house] += elf * multiply;
+
+                if (houses[house] >= input)
+                    results.Add(house);
+
+                visits++;
+            }
+        }
+
+        return results.Min();
+    }
+}
