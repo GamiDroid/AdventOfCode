5375e596255b5ecf73afb8059e16342f08f7d337
Kevin Damen <k.damen@bellt.nl>
2024-01-29 20:29:05 +0100

    Implements challenge day 6 of year 2023

---
+++ AdventOfCode.Tests/2023/Day06_WaitForItTests.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode._2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests._2023;
+public class Day06_WaitForItTests
+{
+    [Theory]
+    [InlineData(0, 7, 0)]
+    [InlineData(1, 7, 6)]
+    [InlineData(2, 7, 10)]
+    [InlineData(3, 7, 12)]
+    [InlineData(4, 7, 12)]
+    [InlineData(5, 7, 10)]
+    [InlineData(6, 7, 6)]
+    [InlineData(7, 7, 0)]
+    public void CalculateDistance_ShouldResultTheExpectedDistanceWhenTheChargeIsHold(int timeHold, int totalTime, ulong expected)
+    {
+        // arrange
+
+        // act
+        var actual = Day06_WaitForIt.CalculateDistance(timeHold, totalTime);
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(7, 9, 4)]
+    [InlineData(15, 40, 8)]
+    [InlineData(30, 200, 9)]
+    [InlineData(71530, 940200, 71503)]
+    public void CountWins_ShouldReturnAmountOfPossibleWins(int time, ulong record, int expected)
+    {
+        // act
+        var actual = Day06_WaitForIt.CountWins(time, record);
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+}
---
+++ AdventOfCode/2023/Day06_WaitForIt.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2023;
+[Challenge(2023, 6, "Wait For It")]
+internal class Day06_WaitForIt
+{
+    private static readonly int[][] s_races = [
+        [46, 214], 
+        [80, 1177], 
+        [78, 1402], 
+        [66, 1024]
+    ];
+
+    [Part(1)]
+    public static void Part01()
+    {
+        int result = 1;
+
+        // for each race...
+        for (int r = 0; r < s_races.Length; r++)
+        {
+            var time = s_races[r][0];
+            var recordDistance = s_races[r][1];
+
+            int winCounter = CountWins(time, (ulong)recordDistance);
+
+            result *= winCounter;
+        }
+
+        Console.WriteLine($"Result part 1: {result}");
+    }
+
+    [Part(1)]
+    public static void Part02()
+    {
+        int result = CountWins(time: 46807866, recordDistance: 214117714021024);
+        Console.WriteLine($"Result part 2: {result}");
+    }
+
+    public static int CountWins(int time, ulong recordDistance)
+    {
+        int minTimeHoldToWin = -1;
+        int maxTimeHoldToWin = -1;
+
+        for (int i = 1; i < time; i++)
+        {
+            if (minTimeHoldToWin != -1 && maxTimeHoldToWin != -1)
+                break;
+
+            var distance = CalculateDistance(i, time);
+            if (minTimeHoldToWin == -1)
+            {
+                // find min
+                if (distance > recordDistance)
+                {
+                    minTimeHoldToWin = i;
+                }
+            }
+            else if (maxTimeHoldToWin == -1)
+            {
+                // find max
+                if (distance < recordDistance)
+                {
+                    maxTimeHoldToWin = i;
+                }
+            }
+        }
+
+        return maxTimeHoldToWin - minTimeHoldToWin;
+    }
+
+    public static ulong CalculateDistance(int chargeHoldTime, int totalRaceTime)
+    {
+        // not charged...
+        if (chargeHoldTime <= 0)
+            return 0;
+
+        var timeToTravel = totalRaceTime - chargeHoldTime;
+        // no time to travel...
+        if (timeToTravel <= 0)
+            return 0;
+
+        return (ulong)chargeHoldTime * (ulong)timeToTravel;
+    }
+}
