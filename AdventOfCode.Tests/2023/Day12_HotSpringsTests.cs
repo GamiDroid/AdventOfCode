ce4050c080a70bd57599be639ddb9d16506ad035
Kevin Damen <k.damen@bellt.nl>
2024-11-03 21:09:05 +0100

    save changes in hot springs

---
+++ AdventOfCode.Tests/2023/Day12_HotSpringsTests.cs
@@ -0,0 +1,83 @@
+﻿using AdventOfCode._2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Tests._2023;
+public class Day12_HotSpringsTests
+{
+    // 3,2,1
+    // ?###????????
+    // 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1
+    // ?###??????????###??????????###??????????###??????????###????????
+
+    [Theory]
+    [InlineData(".#.", "#")]
+    [InlineData("..#..", "#")]
+    [InlineData("#..", "#")]
+    [InlineData("..#", "#")]
+    [InlineData("..#.#..", "#.#")]
+    public void SpringSolver_Trim_Should_Remove_Hot_Springs_From_Front_And_End(string springs, string expected)
+    {
+        // arrange
+        var solver = new Day12_HotSprings.SpringSolver(springs, []);
+
+        // act
+        solver.Trim();
+        var actual = solver.GetSprings();
+
+        // assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("#??", "1", "#.?")]
+    [InlineData("#??", "2", "##.")]
+    [InlineData("#??", "3", "###")]
+    [InlineData("#?#?", "1,2", "#.##")]
+    [InlineData(".#?", "1", ".#.")]
+    [InlineData("?#?", "1", "?#.")]
+    [InlineData(".#?", "2", ".##")]
+    [InlineData(".??..??...?##.", "1,1,3", ".??..??...?##.")]
+    [InlineData("?#?#?#?#?#?#?#?", "1,3,1,6", "?#.###.#.######")]
+    [InlineData("???#?#?#?#?#?#?", "1,3,1,6", "???#?#?#?#?#?#?")]
+    public void SpringSolver_TrySolveLeftToRight_Should_Try_Fill_From_Left_To_Right(string springs, string groupsAsString, string expected)
+    {
+        // arrange
+        var groups = groupsAsString.Split(',').Select(x => int.Parse(x)).ToArray();
+        var solver = new Day12_HotSprings.SpringSolver(springs, groups);
+
+        // act
+        solver.TrySolveLeftToRight();
+        var actual = solver.GetSprings();
+        // assert
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("??#", "1", "?.#")]
+    [InlineData("??#", "2", ".##")]
+    [InlineData("??#", "3", "###")]
+    [InlineData("?#?#", "2,1", "##.#")]
+    [InlineData("?#.", "1", ".#.")]
+    [InlineData("?#?", "1", ".#?")]
+    [InlineData("?#.", "2", "##.")]
+    [InlineData(".??..??...?##.", "1,1,3", ".??..??...###.")]
+    [InlineData("???#?#?#?#?#?#?", "1,3,1,6", "???#?#?#?#####?")]
+    public void SpringSolver_TrySolveRightToLeft_Should_Try_Fill_From_Right_To_Left(string springs, string groupsAsString, string expected)
+    {
+        // arrange
+        var groups = groupsAsString.Split(',').Select(x => int.Parse(x)).ToArray();
+        var solver = new Day12_HotSprings.SpringSolver(springs, groups);
+
+        // act
+        solver.TrySolveRightToLeft();
+        var actual = solver.GetSprings();
+        // assert
+
+        Assert.Equal(expected, actual);
+    }
+}
--- AdventOfCode/2023/Day12_HotSprings.cs
+++ AdventOfCode/2023/Day12_HotSprings.cs
@@ -27,6 +27,15 @@ internal class Day12_HotSprings
         Console.WriteLine($"Sum of diffent arrangements is {sum}");
     }
 
+    [Part(2)]
+    public void Part02()
+    {
+        var sum = _springRecords
+            .Select(x => (Springs: string.Join("?", Enumerable.Repeat(x.Springs, 5)), Groups: Enumerable.Repeat(x.Groups, 5).SelectMany(x => x).ToArray()))
+            .Sum(x => FindAmountOfDifferentArrangements(x.Springs, x.Groups));
+        Console.WriteLine($"Sum of diffent arrangements is {sum}");
+    }
+
     internal static int FindAmountOfDifferentArrangements(string springs, int[] groups)
     {
         int springsCount = springs.Length;
@@ -36,13 +45,37 @@ internal class Day12_HotSprings
             return 1;
 
         var damagedSpringsCount = springs.Count(x => x == '#');
-        var damagedSpringsCountNeeded = groups.Sum();
-
-        if (damagedSpringsCount == damagedSpringsCountNeeded)
+        if (damagedSpringsCount == groups.Sum())
             return 1;
 
-        var damagedSpringsMissingCount = damagedSpringsCountNeeded - damagedSpringsCount;
+        SpringSolver solver = new(springs, groups);
+
+        bool solvedSpring;
+        do
+        {
+            solvedSpring = false;
+
+            Console.WriteLine($"{string.Join(',', groups)}: ");
+            Console.WriteLine($" {solver.GetSprings()}");
+
+            Console.WriteLine("left to right:");
+            solvedSpring = solvedSpring || solver.TrySolveLeftToRight();
+            Console.WriteLine($"- {solver.GetSprings()}");
+
+            Console.WriteLine("right to left :");
+            solvedSpring = solvedSpring || solver.TrySolveRightToLeft();
+            Console.WriteLine($"+ {solver.GetSprings()}");
+
+        } while (solvedSpring);
+
+        springs = solver.GetSprings();
+        groups = solver.GetGroups();
+
+        damagedSpringsCount = springs.Count(x => x == '#');
+        var damagedSpringsMissingCount = groups.Sum() - damagedSpringsCount;
         var unknownSpringLocations = Regex.Matches(springs, "\\?").Select(m => m.Index).ToArray();
+
+        Console.WriteLine("Force solve!");
 
         List<List<int>> combinations = [];
         FindCombinations(unknownSpringLocations, damagedSpringsMissingCount, ref combinations);
@@ -50,20 +83,27 @@ internal class Day12_HotSprings
         int matches = 0;
         foreach (var combination in combinations)
         {
-            var springsAsArr = springs.ToCharArray();
-            foreach (var i in combination)
-            {
-                springsAsArr[i] = '#';
-            }
-
-            var ranges = FindDamagedSpringGroups(new string(springsAsArr)).Select(x => x.Length).ToArray();
+            var newSprings = SetSprings(springs, combination, '#');
+            var ranges = FindDamagedSpringGroups(newSprings).Select(x => x.Length).ToArray();
             if (groups.SequenceEqual(ranges))
             {
                 matches++;
             }
         }
 
+        Console.WriteLine($"{springs} {string.Join(',', groups)} - {matches}");
+
         return matches;
+    }
+
+    private static string SetSprings(string springs, IEnumerable<int> indexes, char springKind)
+    {
+        var springsAsArr = springs.ToCharArray();
+        foreach (var i in indexes)
+        {
+            springsAsArr[i] = '#';
+        }
+        return new string(springsAsArr);
     }
 
     private static List<GroupRange> FindDamagedSpringGroups(string springs)
@@ -100,11 +140,156 @@ internal class Day12_HotSprings
         {
             Start = start;
             Length = length;
-            End = start + length;
+            End = start + length - 1;
         }
 
         public int Start { get; }
         public int End { get; }
         public int Length { get; }
+    }
+
+    public class SpringSolver(string springs, int[] groups)
+    {
+        private readonly char[] _springs = springs.ToCharArray();
+        private readonly List<int> _groups = [..groups];
+        private int _start = 0;
+        private int _end = springs.Length - 1;
+
+        public int Length => _end - _start + 1;
+
+        public void TrySolveLargestGroups()
+        {
+            var damagedGroups = FindDamagedSpringGroups(new(_springs));
+            foreach (var gl in _groups.Distinct().OrderDescending())
+            {
+                bool hasFound = false;
+
+                foreach (var g in damagedGroups.Where(x => x.Length == gl))
+                {
+                    hasFound = true;
+                    RangeSet(_springs, g.Start-1, g.End+1, '.');
+                }
+
+                if (!hasFound)
+                    break;
+
+                _groups.RemoveAll(x => x == gl);
+            }
+        }
+
+        public bool TrySolveLeftToRight()
+        {
+            char[] startSprings = [.._springs];
+
+            int start = _start;
+            for (int gi = 0; gi < _groups.Count; gi++)
+            {
+                var group = _groups[gi];
+                var springIndex = Array.IndexOf(_springs, '#', start);
+
+                if (springIndex == -1 || springIndex-start > group) 
+                {
+                    break;
+                }
+
+                var unknownSpringCount = _springs
+                    .Select((c, i) => (Spring: c, Index: i))
+                    .Count(x => x.Index >= start && x.Index < springIndex && x.Spring == '?');
+                var correction = group > 1 ? unknownSpringCount : 0;
+
+                var rangeLength = RangeSet(_springs, springIndex, springIndex + (group - correction) - 1, '#');
+
+                if (rangeLength < group || _springs.Length <= springIndex + group)
+                {
+                    break;
+                }
+                _springs[springIndex + group] = '.';
+
+                start = springIndex + group;
+            }
+
+            return !startSprings.SequenceEqual(_springs);
+        }
+
+        public bool TrySolveRightToLeft()
+        {
+            char[] startSprings = [.. _springs];
+
+            int start = _end;
+            for (int gi = _groups.Count - 1; gi >= 0; gi--)
+            {
+                var group = _groups[gi];
+                var springIndex = Array.LastIndexOf(_springs, '#', start);
+
+                if (springIndex == -1 || springIndex < _start || (start - springIndex) > group)
+                {
+                    break;
+                }
+
+                var unknownSpringCount = _springs
+                    .Select((c, i) => (Spring: c, Index: i))
+                    .Count(x => x.Index <= start && x.Index > springIndex && x.Spring == '?');
+                var correction = group > 1 ? unknownSpringCount : 0;
+
+                var startRange = springIndex - (group - correction) + 1;
+                var rangeLength = RangeSet(_springs, startRange, springIndex, '#');
+                if (rangeLength < group || springIndex - group < 0)
+                {
+                    break;
+                }
+                
+                _springs[springIndex - group] = '.';
+                start = springIndex - group;
+            }
+
+            return !startSprings.SequenceEqual(_springs);
+        }
+
+        public void Trim()
+        {
+            TrimLeft();
+            TrimRight();
+        }
+
+        private void TrimLeft()
+        {
+            for (int i = _start; i <= _end; i++)
+            {
+                _start = i;
+                if (_springs[i] != '.')
+                {
+                    break;
+                }
+            }
+        }
+
+        private void TrimRight()
+        {
+            for (int i = _end; i >= _start; i--)
+            {
+                _end = i;
+                if (_springs[i] != '.')
+                {
+                    break;
+                }
+            }
+        }
+
+        private static int RangeSet(char[] spring, int start, int end, char value)
+        {
+            start = int.Max(start, 0);
+            end = int.Min(end, spring.Length-1);
+
+            for (int i = start; i <= end; i++)
+                spring[i] = value;
+            return end - start + 1;
+        }
+
+        public string GetSprings()
+        {
+            return new string(_springs, _start, Length);
+        }
+
+        public int[] GetGroups() => [.._groups];
     }
 }
