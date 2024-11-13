c80d00d6e0302a3481773dc2a221eaa4f4574801
Kevin Damen <k.damen@bellt.nl>
2024-11-13 07:53:16 +0100

    implemented part 1 of day 13 year 2023
    I understood the assignment not completely. I thought I couldn't start at index 0.

--- AdventOfCode/2023/Day13_PointofIncidence.cs
+++ AdventOfCode/2023/Day13_PointofIncidence.cs
@@ -35,120 +35,47 @@ internal class Day13_PointofIncidence
         _patterns = [.. tempPatterns];
     }
 
-    private (int Left, int Top) StartCursor;
-    private (int Left, int Top) EndCursor;
-
     [Part(1)]
     public void Part01()
     {
         var sum = 0;
-        int patternCount = 1;
         foreach (var pattern in _patterns)
         {
-            // pattern[x] is row x
-            // pattern[x][y] is cell column x, row y
-
-            StartCursor = Console.GetCursorPosition();
-            PrintPattern(pattern);
-            EndCursor = Console.GetCursorPosition();
-
             var verticalMirror = FindVerticalReflection(pattern);
             sum += verticalMirror;
             
             var horizontalMirror = FindHorizontalReflection(pattern);
             sum += horizontalMirror * 100;
-
-            Console.WriteLine($"v: {verticalMirror} h: {horizontalMirror}");
-
-            if (verticalMirror == 0 &&  horizontalMirror == 0)
-                Console.WriteLine($"{patternCount} does not have a mirror");
-            patternCount++;
         }
 
         Console.WriteLine($"Sum is {sum}");
     }
 
-    private void PrintPattern(char[][] pattern)
+    private static int FindVerticalReflection(char[][] pattern)
     {
-        foreach (var line in pattern)
+        for (int col1 = 0; col1 < pattern[0].Length; col1++)
         {
-            Console.WriteLine(line);
-        }
-    }
-
-    private int FindVerticalReflection(char[][] pattern)
-    {
-        int start = 0;
-        if (pattern[0].Length % 2 != 0)
-            start = 1;
-
-        for (int col1 = start; col1 < pattern[0].Length; col1++)
-        {
-            HighlightColumn(pattern, col1);
             for (int col2 = col1 + 1;  col2 < pattern[0].Length; col2++)
             {
-                HighlightColumn(pattern, col2);
-
                 if (ColumnEqual(pattern, col1, col2))
                 {
                     var distance = col2 - col1;
                     var distanceToMirror = distance / 2;
-
-                    Console.WriteLine($"found same col at {col1} and {col2}");
-
-
-                    Console.WriteLine($"Check vertical mirror at rows {col1 + distanceToMirror} {col2 - distanceToMirror}");
 
                     if (IsMirroredVertically(pattern, col1 + distanceToMirror, col2 - distanceToMirror))
                     {
                         return col1 + distanceToMirror + 1;
                     }
                 }
-
-                ResetColumn(pattern, col2);
-
-                Thread.Sleep(1000);
             }
-
-            ResetColumn(pattern, col1);
         }
 
         return 0;
     }
 
-    private void HighlightColumn(char[][] pattern, int column)
+    private static int FindHorizontalReflection(char[][] pattern)
     {
-        Console.BackgroundColor = ConsoleColor.Cyan;
-        Console.ForegroundColor = ConsoleColor.Black;
-
-        ReprintColumn(pattern, column);
-        Console.ResetColor();
-    }
-
-    private void ResetColumn(char[][] pattern, int column)
-    {
-        Console.ResetColor();
-        ReprintColumn(pattern, column);
-    }
-
-    private void ReprintColumn(char[][] pattern, int column)
-    {
-        for (int row = 0; row < pattern.Length; row++)
-        {
-            Console.CursorLeft = StartCursor.Left + column;
-            Console.CursorTop = StartCursor.Top + row - 1;
-
-            Console.Write(pattern[row][column]);
-        }
-    }
-
-    private int FindHorizontalReflection(char[][] pattern)
-    {
-        int start = 0;
-        if (pattern.Length % 2 != 0)
-            start = 1;
-
-        for (int row1 = start; row1 < pattern.Length; row1++)
+        for (int row1 = 0; row1 < pattern.Length; row1++)
         {
             for (int row2 = row1 + 1; row2 < pattern.Length; row2++)
             {
@@ -156,10 +83,6 @@ internal class Day13_PointofIncidence
                 {
                     var distance = row2 - row1;
                     var distanceToMirror = distance / 2;
-
-                    Console.WriteLine($"found same row at {row1} and {row2}");
-
-                    Console.WriteLine($"Check horizontal mirror at rows {row1 + distanceToMirror} {row2 - distanceToMirror}");
 
                     if (IsMirroredHorizontally(pattern, row1 + distanceToMirror, row2 - distanceToMirror))
                     {
@@ -178,7 +101,6 @@ internal class Day13_PointofIncidence
         {
             if (!ColumnEqual(pattern, col1, col2))
             {
-                Console.WriteLine($"not equal between column {col1} and {col2}");
                 return false;
             }
             col1--;
@@ -195,7 +117,6 @@ internal class Day13_PointofIncidence
         {
             if (!RowEqual(pattern, row1, row2))
             {
-                Console.WriteLine($"not equal between row {row1} and {row2}");
                 return false;
             }
             row1--;
