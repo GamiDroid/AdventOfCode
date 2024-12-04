9d53cc5b624332291c4987dd96e421bf9300950c
Kevin Damen <k.damen@bellt.nl>
2024-12-04 21:20:46 +0100

    solved day 4 of 2024

---
+++ AdventOfCode/2024/Day04_CeresSearch.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AdventOfCode._2024;
+
+[Challenge(2024, 4, "Ceres Search")]
+internal class Day04_CeresSearch
+{
+    private readonly Map<char> _map;
+
+    public Day04_CeresSearch()
+    {
+        var lines = ChallengeHelper.ReadAllLinesFromResourceFile();
+        _map = Map.Create(lines);
+    }
+
+    [Part(1)]
+    public void Part1()
+    {
+        List<string> strings = [];
+
+        strings.AddRange(Enumerable.Range(0, _map.LengthX).Select(GetColumn));
+        strings.AddRange(Enumerable.Range(0, _map.LengthY).Select(GetRow));
+        strings.AddRange(Enumerable.Range(0, _map.LengthX - 3).Select(x => GetForwardSlope(x, 0)));
+        strings.AddRange(Enumerable.Range(0, _map.LengthY - 3).Skip(1).Select(y => GetForwardSlope(0, y)));
+        strings.AddRange(Enumerable.Range(0, _map.LengthX).Skip(3).Select(x => GetBackwardSlope(x, 0)));
+        strings.AddRange(Enumerable.Range(0, _map.LengthY-3).Skip(1).Select(y => GetBackwardSlope(_map.LengthX-1, y)));
+
+        var sum = strings.Sum(x => Regex.Matches(x, "XMAS").Count);
+        var sumReversed = strings.Select(x => new string(x.Reverse().ToArray())).Sum(x => Regex.Matches(x, "XMAS").Count);
+
+        var answer = sum + sumReversed;
+        Console.WriteLine($"Part 1: {answer}");
+    }
+
+    [Part(2)]
+    public void Part2()
+    {
+        int count = 0;
+        for (int x = 0; x < _map.LengthX-2; x++)
+        {
+            for (int y = 0; y < _map.LengthY-2; y++)
+            {
+                if (IsCrossMAS(x, y) || 
+                    IsCrossSAM(x, y) ||
+                    IsCrossMASVertical(x, y) ||
+                    IsCrossSAMVertical(x, y))
+                    count++;
+            }
+        }
+
+        Console.WriteLine($"Part 2: {count}");
+    }
+
+    #region Part 2 helpers
+    private bool IsCrossMAS(int x, int y)
+    {
+        return (_map[x, y] == 'M' &&
+            _map[x + 2, y] == 'S' &&
+            _map[x, y + 2] == 'M' &&
+            _map[x + 2, y + 2] == 'S' &&
+            _map[x + 1, y + 1] == 'A');
+    }
+
+    private bool IsCrossMASVertical(int x, int y)
+    {
+        return (_map[x, y] == 'M' &&
+            _map[x + 2, y] == 'M' &&
+            _map[x, y + 2] == 'S' &&
+            _map[x + 2, y + 2] == 'S' &&
+            _map[x + 1, y + 1] == 'A');
+    }
+
+    private bool IsCrossSAM(int x, int y)
+    {
+        return (_map[x, y] == 'S' &&
+            _map[x + 2, y] == 'M' &&
+            _map[x, y + 2] == 'S' &&
+            _map[x + 2, y + 2] == 'M' &&
+            _map[x + 1, y + 1] == 'A');
+    }
+
+    private bool IsCrossSAMVertical(int x, int y)
+    {
+        return (_map[x, y] == 'S' &&
+            _map[x + 2, y] == 'S' &&
+            _map[x, y + 2] == 'M' &&
+            _map[x + 2, y + 2] == 'M' &&
+            _map[x + 1, y + 1] == 'A');
+    }
+    #endregion
+    
+    #region Part 1 helpers
+    private string GetRow(int y)
+    {
+        List<char> chars = [];
+        for (int x = 0; x < _map.LengthX; x++)
+        {
+            chars.Add(_map[x, y]);
+        }
+        return new string([.. chars]);
+    }
+
+    private string GetColumn(int x)
+    {
+        List<char> chars = [];
+        for (int y = 0; y < _map.LengthY; y++)
+        {
+            chars.Add(_map[x, y]);
+        }
+        return new string([.. chars]);
+    }
+
+    private string GetForwardSlope(int x, int y)
+    {
+        List<char> chars = [];
+        while (y <= _map.LengthY - 1 && x <= _map.LengthX - 1)
+        {
+            chars.Add(_map[x++, y++]);
+        }
+        return new string([.. chars]);
+    }
+
+    private string GetBackwardSlope(int x, int y)
+    {
+        List<char> chars = [];
+        while (x >= 0 && y <= _map.LengthY - 1)
+        {
+            chars.Add(_map[x--, y++]);
+        }
+        return new string([.. chars]);
+    } 
+    #endregion
+}
--- AdventOfCode/Map.cs
+++ AdventOfCode/Map.cs
@@ -1,32 +1,27 @@
 ﻿namespace AdventOfCode;
-internal class Map
+internal class Map<T> where T : struct
 {
-    private readonly char[,] _map;
+    private readonly T[,] _map;
     private readonly int _lengthX;
     private readonly int _lengthY;
 
-    public Map(string[] strings)
+    public Map(T[,] map)
     {
-        var map = new char[strings[0].Length, strings.Length];
-
-        for (int y = 0; y < strings.Length; y++)
-        {
-            for (int x = 0; x < strings[y].Length; x++)
-            {
-                map[x, y] = strings[y][x];
-            }
-        }
-
         _map = map;
         _lengthX = _map.GetLength(0);
         _lengthY = _map.GetLength(1);
     }
 
+    //public Map(T[][] map)
+    //{
+    //    var tempMap = new();
+    //}
+
     public int LengthX => _lengthX;
     public int LengthY => _lengthY;
 
-    public char this[int x, int y] => _map[x, y];
-    public char this[Location location] => _map[location.X, location.Y];
+    public T this[int x, int y] => _map[x, y];
+    public T this[Location location] => _map[location.X, location.Y];
 
     private bool IsValidLocation(int x, int y)
     {
@@ -39,5 +34,23 @@ internal class Map
     public bool IsValidLocation(Location location)
     {
         return IsValidLocation(location.X, location.Y);
+    }
+}
+
+internal static class Map
+{
+    public static Map<char> Create(string[] strings)
+    {
+        var map = new char[strings[0].Length, strings.Length];
+
+        for (int y = 0; y < strings.Length; y++)
+        {
+            for (int x = 0; x < strings[y].Length; x++)
+            {
+                map[x, y] = strings[y][x];
+            }
+        }
+
+        return new Map<char>(map);
     }
 }
