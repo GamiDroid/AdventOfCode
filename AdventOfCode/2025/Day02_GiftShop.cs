f9035db89f8ca0edf0edeb59db3fb68f8091cba0
GamiDroid <kevin.d.damen@gmail.com>
2025-12-02 21:41:27 +0100

    Solve puzzle year 2025 day 2 part 2

--- AdventOfCode/2025/Day02_GiftShop.cs
+++ AdventOfCode/2025/Day02_GiftShop.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace AdventOfCode._2025;
 
@@ -54,12 +57,7 @@ internal class Day02_GiftShop
         {
             for (var num = start; num <= end; num++)
             {
-                var numAsSpan = num.ToString().AsSpan();
-
-                var firstHalf = numAsSpan[..(numAsSpan.Length / 2)];
-                var secondHalf = numAsSpan[(numAsSpan.Length / 2)..];
-
-                if (firstHalf.Equals(secondHalf, StringComparison.Ordinal))
+                if (!IsValid(num))
                 {
                     sum += num;
                 }
@@ -67,5 +65,49 @@ internal class Day02_GiftShop
         }
 
         Console.WriteLine($"Sum of invalid IDs is {sum}");
+    }
+
+    private static bool IsValid(long num)
+    {
+        if (num < 10)
+            return true;
+
+        var numAsSpan = num.ToString().AsSpan();
+
+        var numLength = numAsSpan.Length;
+
+        if (numAsSpan.Length % 2 != 0)
+        {
+            // uneven
+            var compareAllSameNums = new string(numAsSpan[0], numLength);
+            if (numAsSpan.Equals(compareAllSameNums, StringComparison.Ordinal))
+                return false;
+        }
+
+        for (var seqLen = numLength / 2; seqLen > 0; seqLen--)
+        {
+            if (numLength % seqLen != 0)
+                continue;
+
+            var sequence = numAsSpan[..seqLen];
+            var parts = numLength / seqLen;
+
+            var compareNum = StringCreate(sequence, parts);
+            if (numAsSpan.Equals(compareNum, StringComparison.Ordinal))
+                return false;
+        }
+
+        return true;
+    }
+
+    private static string StringCreate(ReadOnlySpan<char> chars, int count)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < count; i++)
+        {
+            sb.Append(chars);
+        }
+
+        return sb.ToString();
     }
 }
