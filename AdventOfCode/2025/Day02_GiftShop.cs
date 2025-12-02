038ed9543b77b3da75816615bc0c33889615ac5d
GamiDroid <kevin.d.damen@gmail.com>
2025-12-02 08:32:17 +0100

    Solve puzzle year 2025 day 2 part 1

---
+++ AdventOfCode/2025/Day02_GiftShop.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace AdventOfCode._2025;
+
+[Challenge(2025, 2, "Gift Shop")]
+internal class Day02_GiftShop
+{
+    private readonly (long Start, long End)[] _ranges;
+
+    public Day02_GiftShop()
+    {
+        var input = ChallengeHelper.ReadAllTextFromResourceFile();
+
+        _ranges = [.. input
+            .Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(x => x.Split("-", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+            .Select(x => (long.Parse(x[0]), long.Parse(x[1])))];
+    }
+
+    [Part(1)]
+    public void Part1()
+    {
+        long sum = 0;
+
+        foreach (var (start, end) in _ranges)
+        {
+            for (var num = start; num <= end; num++)
+            {
+                var numAsSpan = num.ToString().AsSpan();
+
+                // number has not even length
+                if (numAsSpan.Length % 2 == 1)
+                    continue;
+
+                var firstHalf = numAsSpan[..(numAsSpan.Length / 2)];
+                var secondHalf = numAsSpan[(numAsSpan.Length / 2)..];
+
+                if (firstHalf.Equals(secondHalf, StringComparison.Ordinal))
+                {
+                    sum += num;
+                }
+            }
+        }
+
+        Console.WriteLine($"Sum of invalid IDs is {sum}");
+    }
+
+    [Part(2)]
+    public void Part2()
+    {
+        long sum = 0;
+
+        foreach (var (start, end) in _ranges)
+        {
+            for (var num = start; num <= end; num++)
+            {
+                var numAsSpan = num.ToString().AsSpan();
+
+                var firstHalf = numAsSpan[..(numAsSpan.Length / 2)];
+                var secondHalf = numAsSpan[(numAsSpan.Length / 2)..];
+
+                if (firstHalf.Equals(secondHalf, StringComparison.Ordinal))
+                {
+                    sum += num;
+                }
+            }
+        }
+
+        Console.WriteLine($"Sum of invalid IDs is {sum}");
+    }
+}
